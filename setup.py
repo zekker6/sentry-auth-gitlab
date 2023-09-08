19d04113dfac4645c563e2981ecdcb4bb25b3113
Zakhar Bessarab <zekker6@gmail.com>
2023-09-08 15:20:15 +0400

    update package version to 0.3.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open(HERE + "/README.rst", 'r') as f:
 
 setup(
     name='sentry-auth-gitlab-v2',
-    version='0.2.4',
+    version='0.3.0',
     author='Zakhar Bessarab',
     author_email='zekker6@gmail.com',
     url='https://github.com/zekker6',
