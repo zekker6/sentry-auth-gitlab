17a7a91fd8cc23d85206ef6af191db9f051f4d76
Zakhar Bessarab <zekker6@gmail.com>
2024-07-13 13:43:56 +0400

    fix an ability to override scheme
    It was forced to be https since 0.4.0.

--- auth_gitlab/constants.py
+++ auth_gitlab/constants.py
@@ -6,7 +6,8 @@ CLIENT_SECRET = settings.GITLAB_APP_SECRET
 SCOPE = getattr(settings, 'GITLAB_AUTH_SCOPE', 'api')
 
 BASE_DOMAIN = settings.GITLAB_BASE_DOMAIN
+BASE_SCHEME = settings.GITLAB_HTTP_SCHEME
 
-ACCESS_TOKEN_URL = f"https://{BASE_DOMAIN}/oauth/token"
-AUTHORIZE_URL = f"https://{BASE_DOMAIN}/oauth/authorize"
-API_ENDPOINT = f"https://{BASE_DOMAIN}/api/v4"
+ACCESS_TOKEN_URL = f"{BASE_SCHEME}://{BASE_DOMAIN}/oauth/token"
+AUTHORIZE_URL = f"{BASE_SCHEME}://{BASE_DOMAIN}/oauth/authorize"
+API_ENDPOINT = f"{BASE_SCHEME}://{BASE_DOMAIN}/api/v4"
